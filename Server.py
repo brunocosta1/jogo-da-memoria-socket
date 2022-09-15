cbcd0bba719f411c97835a1ee4c92af804aa9a0f
brunocosta1 <brunocosta.cg1998@gmail.com>
2022-09-15 13:34:46 -0300

    Adicionando uma forma de pegar os argumentos da linha de comando

--- Server.py
+++ Server.py
@@ -1,5 +1,12 @@
 import socket
 from threading import Thread
+import argparse
+
+parser = argparse.ArgumentParser(description='Servidor que vai esperar os jogadores')
+parser.add_argument('--numero', type=int, help='Indica o número de jogadores')
+parser.add_argument('--porta', type=str, help='Indica em que porta vai escutar')
+parser.add_argument('--host', type=str, help='Indica qual vai ser o host do server')
+args = parser.parse_args()
 
 users: socket.socket = []
 
