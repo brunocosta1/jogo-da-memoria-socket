efd53fb81041bda9c0928a00cba567a22d7d09d1
brunocosta1 <brunocosta.cg1998@gmail.com>
2022-09-16 00:28:25 -0300

    Trocando o tipo de variável da porta

--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Servidor que vai esperar os jogadores')
 parser.add_argument('--numero', type=int, help='Indica o número de jogadores')
-parser.add_argument('--porta', type=str, help='Indica em que porta vai escutar')
+parser.add_argument('--porta', type=int, help='Indica em que porta vai escutar')
 parser.add_argument('--host', type=str, help='Indica qual vai ser o host do server')
 args = parser.parse_args()
 
@@ -22,7 +22,7 @@ def handle(client):
             break
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("", 9803))
+server.bind(("", args.porta))
 server.listen()
 
 print("SERVER STARTED")
