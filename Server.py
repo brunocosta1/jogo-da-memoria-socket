9f8c82db9af18a19b2fca0d3267a1a91f0e158ea
brunocosta1 <brunocosta.cg1998@gmail.com>
2022-09-18 11:39:40 -0300

    Implementando a lógica da conexão do início do jogo

--- Server.py
+++ Server.py
@@ -1,35 +1,47 @@
 import socket
-from threading import Thread
 import argparse
+from typing import List
 
-parser = argparse.ArgumentParser(description='Servidor que vai esperar os jogadores')
-parser.add_argument('--numero', type=int, help='Indica o número de jogadores')
-parser.add_argument('--porta', type=int, help='Indica em que porta vai escutar')
-parser.add_argument('--host', type=str, help='Indica qual vai ser o host do server')
-args = parser.parse_args()
+def getArgs():
+    parser = argparse.ArgumentParser(description='Servidor que vai esperar os jogadores')
+    parser.add_argument('--numero', type=int, help='Indica o número de jogadores')
+    parser.add_argument('--porta', type=int, help='Indica em que porta vai escutar')
+    parser.add_argument('--host', type=str, help='Indica qual vai ser o host do server')
+    return parser.parse_args()
 
-users: socket.socket = []
 
-def handle(client):
-    while True:
-        try:
-            msg = client.recv(1024).decode("utf-8")
-            for user in users:
-                user.send(msg.encode("utf-8"))
-        except:
-            users.remove(client)
-            client.close()
-            break
+# Função para fechar a conexão
+def fechaConexao(users: List[socket.socket], server: socket.socket):
+    for user in users:
+        user.close()
+    server.close()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("", args.porta))
-server.listen()
 
-print("SERVER STARTED")
 
-while True:
-    client, address = server.accept()
-    users.append(client)
-    print("[CONNECTION]: ", address)
 
-    Thread(target = handle, args = (client,)).start()
+# Conexão dos jogadores
+
+def conexaoJogadores(HOST, PORT, numeroJogadores):
+    print("SERVER STARTED")
+    users: List[socket.socket] = []
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+    while len(users) < numeroJogadores:
+        client, address = server.accept()
+        users.append(client)
+        print(f"Número de jogadores conectados: {len(users)}")
+        print("[CONNECTION]: ", address)
+    print("Todos os jogadores se conectaram!")
+    return users, server
+
+
+def main():
+    args = getArgs()
+    users, server = conexaoJogadores(args.host, args.porta, args.numero)
+    fechaConexao(users, server)
+
+
+if __name__ == "__main__":
+    main()
