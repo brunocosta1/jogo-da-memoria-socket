258aa9dc1607cffd431fd3b0f7af007ed2931c74
brunocosta1 <brunocosta.cg1998@gmail.com>
2022-09-21 14:46:19 -0300

    Adicionando mais um argumento da linha de comando para pegar a dimensão do tabuleiro

--- Server.py
+++ Server.py
@@ -10,6 +10,7 @@ def getArgs():
     parser.add_argument('--numero', type=int, help='Indica o número de jogadores')
     parser.add_argument('--porta', type=int, help='Indica em que porta vai escutar')
     parser.add_argument('--host', type=str, help='Indica qual vai ser o host do server')
+    parser.add_argument('--dimensao', type=int, help='Indica a dimensão do tabuleiro')
     return parser.parse_args()
 
 
